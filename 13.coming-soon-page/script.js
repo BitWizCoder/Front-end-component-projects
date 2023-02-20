9a5a1e728fbaabc316204d771f22034b84ac7912
Muhammad <mhmdnoman01@gmail.com>
2023-02-20 16:00:16 +0600

    new projects

---
+++ 13.coming-soon-page/script.js
@@ -0,0 +1,11 @@
+$('#clock').countdown('2029/10/10', function(event) {
+    var $this = $(this).html(event.strftime(''
+      + '<div class="main">'
+      + '<h1 class="count">%w <span> weeks </span></h1>'
+      + '<h1 class="count">%d <span> days</span></h1>'
+      + '<h1 class="count">%H <span> hr</span></h1>'
+      + '<h1 class="count">%M <span> min</span></h1>'
+      + '<h1 class="count">%S <span> sec</span></h1>'
+      + '<div/>'
+      ));
+  });
